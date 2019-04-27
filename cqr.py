eebf61416f89a348fe2a2d8253e22bc94f973b1b
Bernhard Kneip <bernhard.kneip@postadigitale.de>
2019-04-27 21:00:58 +0200

    in cqrmysql: add_band_data introduced helper: freq_to_band, freq_to_mode

--- cqr.py
+++ cqr.py
@@ -1,4 +1,5 @@
 import pymysql
+from collections import OrderedDict
 class cqrmysql:
 
     def __init__(self, MYSQL_HOST, MYSQL_USER, MYSQL_PASS, MYSQL_DB):
@@ -27,7 +28,7 @@ class cqrmysql:
             #pp.pprint(clusters)
             return(clusters)
         except:
-            print ("Error: unable to fetch data")
+            print ("Error: unable to fetch cluster data")
 
     def get_modes(self):
         try:
@@ -43,6 +44,53 @@ class cqrmysql:
             #pp.pprint(modes)
             return(modes)
         except:
-            print ("Error: unable to fetch data")
+            print ("Error: unable to fetch mode data")
+    
+    def get_band_data(self):
+        try:
+            # Execute the SQL command
+            self.mysql_cursor.execute("SELECT * FROM bands")
+            # Fetch all the rows in a list of lists.
+            results = self.mysql_cursor.fetchall()
+            band_data = dict()
+            for row in results:
+                band = {
+                    "ID"        : row[0],
+                    "NAME"      : row[1],
+                    "F_BEGIN"   : row[2],
+                    "F_END"     : row[3],
+                    "F_CW"      : row[4],
+                    "F_RTTY"    : row[5],
+                    "F_SSB"     : row[6]
+                }
+                band_data[row[0]] = band
+            return(band_data)
+        except:
+            print ("Error: unable to fetch band data")
+    
     def __del__(self):
         self.mysql_conn.close()
+
+class helper:
+    def __init__(self, mode_list, band_data):
+        self.mode_list = mode_list
+        self.band_data = band_data
+    
+    def freq_to_band(self, freq):
+        for id in self.band_data:
+            band = self.band_data[id]
+            if band["F_BEGIN"] <= freq <= band["F_END"]:
+                return(band)
+        return(None)
+        
+
+    def freq_to_mode(self, freq):
+        band = self.freq_to_band(freq)
+        if freq <= band["F_CW"]:
+            return(self.mode_list["CW"])
+        elif band["F_SSB"] >= freq >= band["F_RTTY"]:
+            return(self.mode_list["RTTY"])
+        elif band["F_END"] >= freq >= band["F_SSB"]:
+            return(self.mode_list["SSB"])
+        else:
+            return(None)
